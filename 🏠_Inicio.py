43257a848361f1bf2a2832f26c14b45de2b4be6a
AlanChomina <alan.chomina@uabc.edu.mx>
2025-11-29 18:24:20 -0800

    Inicio del dashboard

---
+++ 🏠_Inicio.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Dashboard Brecha Digital",
+    layout="wide",
+)
+
+st.title("📊 Proyecto Final – Brecha Digital en México")
+
+st.markdown("""
+Bienvenido al dashboard para el analisis de la brecha digital de estudiantes de bajos recuross en México.
+
+Selecciona una página en el menú lateral:
+
+- **📡 Acceso a TICs**  
+- **🎓 Educación & 💼 Empleo**  
+- **👥 Población & 🏠 Vivienda**
+
+""")
